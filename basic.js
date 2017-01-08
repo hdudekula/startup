491965d3fa0049d4e0ce9a0ad123db28e39da8f0
Dudekula <hazi.dudekula@cgi.com>
2017-01-08 10:35:17 -0500

    adding knowmorepages

---
+++ basic.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+      $(".dropdown-button").dropdown();
+	$(".button-collapse").sideNav();
+ 	$('.carousel.carousel-slider').carousel({full_width: true});
+	$('.parallax').parallax();
+    });
